6e0387f15525b2b30b0b9e1d6582053d8b892728
Sameer <sameer.raza9200@gmail.com>
2024-04-10 04:45:06 +0500

    Added favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -149,7 +149,40 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
 
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Text(
+              'You currently have ${appState.favorites.length} favorites.'),
+        ),
+        for (var fav in appState.favorites)
+          Row(
+            children: [
+              SizedBox(width: 15),
+              Flexible(
+                child: ListTile(
+                  title: Text(fav.asPascalCase),
+                  leading: Icon(Icons.favorite),
+                ),
+              ),
+            ],
+          ),
+      ],
+    );
+  }
+}
 
 class BigCard extends StatelessWidget {
   const BigCard({
